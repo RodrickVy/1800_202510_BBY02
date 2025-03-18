a6c4382ba9c6b200932b59b95e8f797371eef1a7
Ken Lee <klee474@my.bcit.ca>
2025-03-17 20:24:46 -0700

    Ken fixed up some grammar on the pages as well as touched up the eachLeague.html page and removed deleted unncessary pages.

--- scripts/league/eachLeague.view.js
+++ scripts/league/eachLeague.view.js
@@ -1,76 +1,56 @@
 
-document.addEventListener("DOMContentLoaded",async (e)=>{
+document.addEventListener("DOMContentLoaded", async (e) => {
 
     const allLeagues = await LeaguesService.getAllLeagues();
-
-
     var leagueToViewCode = localStorage.getItem("leagueToView");
     let currentLeague;
 
-
-    if(leagueToViewCode !== null){
-        leagueToViewCode ;
+    if (leagueToViewCode !== null) {
+        leagueToViewCode;
         allLeagues.forEach(league => {
-            if(league.code === leagueToViewCode){
+            if (league.code === leagueToViewCode) {
                 currentLeague = league;
             }
         });
-    }else {
+    } else {
 
         navigateToRoute(___PAGES.leagues);
     }
-
-
-    loadTemplate('LeagueName',currentLeague.name);
+    loadTemplate('LeagueName', currentLeague.name);
 
     let cityOptions = '';
 
-   
-
-    currentLeague.city.forEach((cityOption)=>{
+    currentLeague.city.forEach((cityOption) => {
 
         cityOptions += `<option value="${cityOption}" id='selectOption_${cityOption.toLowerCase()}'>${cityOption}</option>`;
-
     });
+    loadTemplate('city', cityOptions);
 
-    loadTemplate('city',cityOptions);
-
-
-    
     let levelOptions = '';
 
-   
+    currentLeague.level.forEach((levelOption) => {
 
-    currentLeague.level.forEach((levelOption)=>{
-
-        levelOptions += `<option value="${levelOption}" id='selectOption_${levelOption.toLowerCase().replaceAll("+",'')}'>${toTitleCase(levelOption)}</option>`;
-
+        levelOptions += `<option value="${levelOption}" id='selectOption_${levelOption.toLowerCase().replaceAll("+", '')}'>${toTitleCase(levelOption)}</option>`;
     });
 
-    loadTemplate('skillLevel',levelOptions);
+    loadTemplate('skillLevel', levelOptions);
 
-
-
-
-
-    listenToIfExists(`selectLeagueBtn`,'click',(e)=>{
-        Account.updateUser(Account.userAccount.id,(user)=>{
-             return {
+    listenToIfExists(`selectLeagueBtn`, 'click', (e) => {
+        Account.updateUser(Account.userAccount.id, (user) => {
+            return {
                 league: leagueToViewCode,
                 skillLevel: document.getElementById('skillLevel').value,
                 city: document.getElementById('city').value
-             }
-        } ).then((e)=>{
+            }
+        }).then((e) => {
             navigateToRoute(___PAGES.leagues);
         })
-
     })
 
-   
+
 
 
 })
 
 
 
- 
